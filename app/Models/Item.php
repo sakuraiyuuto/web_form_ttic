bf4ee5ec22e701cd5bbc5a02b211c463585ebfea
Rachman Dwi Putra <cruzher1st@gmail.com>
2022-04-13 21:23:42 +0700

    Menambahkan fungsi pada model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -44,6 +44,92 @@ class Item extends Model
 
     public static function dapatkanNomorInvoiceTerakhir()
     {
-        date("ymd");
+        $invoiceTerakhir = DB::table('tbl_invoice')
+            ->whereBetween('invoice_date', [date('Y-m-d') . ' 00:00:00', date('Y-m-d') . ' 23:59:59'])
+            ->orderBy('invoice_date', 'DESC')
+            ->first();
+
+        if ($invoiceTerakhir == null) {
+            $kodeTerbaru = 'BK' . date('y-m-d') . '0001';
+        }
+
+        //BK2203160009
+        //BK2203160010
+        $nomor_invoice = $invoiceTerakhir->no_invoice;
+        $nomorKodeTerakhir = substr($nomor_invoice, 7, 4);
+
+        $angkaKodeTerbaru = intval($nomorKodeTerakhir) + 1;
+
+        $nomorKodeTerbaru = sprintf("%04d", $angkaKodeTerbaru);
+
+        $kodeTerbaru = 'BK' . date('y-m-d') . $nomorKodeTerbaru;
+
+        return $kodeTerbaru;
+    }
+
+    public static function tambahInvoice(
+        $nomorInvoice,
+        $idPelanggan,
+        $catatan
+    ) {
+        $invoice = DB::table('tbl_invoice')->insert(
+            [
+                'no_invoice' => $nomorInvoice,
+                'id_pelanggan' => $idPelanggan,
+                'id_kurir' => null,
+                'invoice_date' => date("Y-m-d h:i:sa"),
+                'total' => 0,
+                'is_finish' => 1,
+                'is_confirmed' => 0,
+                'is_on_deliver' => 0,
+                'is_pick_up' => 0,
+                'catatan' => $catatan,
+
+            ]
+        );
+
+        return $invoice;
+    }
+
+    public static function tambahOrderMakanan(
+        $idInvoice,
+        $idPelanggan,
+        $namaResto,
+        $itemOrder,
+        $alamatOrder,
+        $detailAlamatOrder,
+        $alamatAntar,
+        $detailAlamat,
+        $catatan,
+        $tarif,
+        $porsiOrder,
+        $hargaSatuanOrder,
+        $idMenu,
+    ) {
+        $orderMakanan = DB::table('tbl_order_makanan')->insert(
+            [
+                'id_invoice' => $idInvoice,
+                'id_pelanggan' => $idPelanggan,
+                'waktu_order' => '',
+                'nama_resto' => $namaResto,
+                'item_order' => $itemOrder,
+                'alamat_order' => $alamatOrder,
+                'detail_alamat_order' => $detailAlamatOrder,
+                'alamat_antar' => $alamatAntar,
+                'detail_alamat' => $detailAlamat,
+                'catatan' => $catatan,
+                'tarif' => $tarif,
+                'is_confirmed' => 1,
+                'is_pending' => 1,
+                'porsi_order' => $porsiOrder,
+                'harga_satuan_order' => $hargaSatuanOrder,
+                'is_finish' => 0,
+                'id_menu' => $idMenu,
+                'lat' => null,
+                'lng' => null,
+            ]
+        );
+
+        return $orderMakanan;
     }
 }
