f9e87f01d9c97e62f1fba9c36b94ab219f83dde8
FarisRuswandi <useridptk270799@gmail.com>
2022-04-13 19:39:11 +0700

    FARIS

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ItemsController;
+use App\Http\Controllers\ItemController;
 
 /*
 |--------------------------------------------------------------------------
