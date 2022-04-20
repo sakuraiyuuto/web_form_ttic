272718f839a5f6b11e8ee1fdb6bd2ea75fbd7211
Rachman Dwi Putra <cruzher1st@gmail.com>
2022-04-20 21:07:29 +0700

    Controller jalan

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -15,8 +15,8 @@ class ItemController extends Controller
      */
     public function index()
     {
-        $items = Item::getItems()->get();
-        $totalItems = Item::getItems()->count();
+        $items = Item::getItems('221')->get();
+        $totalItems = Item::getItems('221')->count();
 
         return view('portal.pesanan.index',  compact(
             'items',
@@ -42,25 +42,52 @@ class ItemController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request);
-        // $arrayValidator = array();
-        // for ($i = 1; $i <= $request->totalItems; $i++) {
-        //     array_push($arrayValidator, "'item_" . $i . "' => 'required'");
-        // }
+        $arrayValidator = array();
+        for ($i = 1; $i <= $request->perulangan; $i++) {
+            array_push($arrayValidator, "'item_" . $i . "' => 'required'");
+        }
 
-        // $validator = Validator::make($request->all(), [
-        //     $arrayValidator
-        // ]);
+        $validator = Validator::make($request->all(), [
+            $arrayValidator
+        ]);
 
-        // if ($validator->fails()) {
-        //     return redirect('/admin/himpunan_mahasiswa')->with('alert', 'Ada kesalahan data, coba lagi.');
-        // } else {
+        if ($validator->fails()) {
+            return redirect('/')->with('alert', 'Ada kesalahan data, coba lagi.');
+        } else {
 
-        //     for ($i = 1; $i <= $request->totalItems; $i++) {
-        //         Item::tambahPesanan();
-        //     }
+            $nomorInvoice  = Item::dapatkanNomorInvoiceTerakhir();
 
-        //     return redirect('/admin/himpunan_mahasiswa')->with('status', 'Himpunan Mahasiswa Berhasil Diubah');
-        // }
+            $invoice = item::tambahInvoice($nomorInvoice, '1', '-', $request->alamat_pengantaran);
+
+            $gerai = Item::getGerai('221');
+
+            $arrayItems = $request->all();
+
+            for ($i = 1; $i <= $request->perulangan; $i++) {
+
+                $idMenu = $arrayItems['id_menu_' . $i];
+
+                $jumlahOrder = $arrayItems['jumlah_order_' . $i];
+
+                $menuGerai = Item::getMenuGerai($idMenu);
+
+                Item::tambahOrderMakanan(
+                    $invoice,
+                    '1',
+                    $gerai->nama_gerai,
+                    $menuGerai->nama_menu,
+                    $gerai->alamat_lengkap,
+                    $gerai->alamat_lengkap,
+                    $request->alamat_pengantaran,
+                    $request->alamat_pengantaran,
+                    '-',
+                    '0',
+                    $jumlahOrder,
+                    $menuGerai->harga,
+                    $idMenu
+                );
+            }
+            return redirect('/')->with('status', 'Himpunan Mahasiswa Berhasil Diubah');
+        }
     }
 }
