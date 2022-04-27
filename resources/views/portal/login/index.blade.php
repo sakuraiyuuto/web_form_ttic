174ef4eaee26a6a7e5f9700aa40f298e9fb5c0f5
FarisRuswandi <useridptk270799@gmail.com>
2022-04-27 20:24:27 +0700

    LOGIN PAGE

---
+++ resources/views/portal/login/index.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link href="{{url('/template/plugins/fontawesome-free/css/all.min.css')}}" rel="stylesheet">
+    <!-- icheck bootstrap -->
+    <link href="{{url('/template/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}" rel="stylesheet">
+    <!-- Theme style -->
+    <link href="{{url('/template/dist/css/adminlte.min.css')}}" rel="stylesheet">
+    <link href="{{url('/images/logo_untan.ico')}}" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+</head>
+
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <!-- /.login-logo -->
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+                <a href="../../index2.html" class="h1"><b>Web Formulir</b> TTIC</a>
+            </div>
+            <div class="card-body">
+                <p class="login-box-msg">Login menggunakan akun google anda sebelum mengisi formulir.</p>
+                
+                <div class="social-auth-links text-center mt-2 mb-3">
+                    <a href="#" class="btn btn-block btn-primary">
+                        <i class="fab fa-google mr-2"></i> Login menggunakan Google
+                    </a>
+                </div>
+                <!-- /.social-auth-links -->
+
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+    </div>
+    <!-- /.login-box -->
+
+    <!-- jQuery -->
+    <script src="../../plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../../dist/js/adminlte.min.js"></script>
+</body>
+
+</html>
